59985a6c2ff24410c80f409c0b0ac6fbf9cd3682
徐浩 <117811771+zyuhao7@users.noreply.github.com>
2026-02-14 03:00:38 +0000

    modify globals

--- src/Ch04/04_03b/globals.h
+++ src/Ch04/04_03b/globals.h
@@ -4,6 +4,6 @@
 #include <iostream>
 
 // Global discount amount defined here (will later be redefined in main.cpp)
-double globalDiscount = 10.0;
+// double globalDiscount = 10.0;
 
 #endif // GLOBALS_H
